4a9ae5898c4bef36cf99feed757657b0520e8e25
kevin <kevinhellman94@gmail.com>
2021-02-27 15:43:26 -0500

    Created SQL File

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { prompt } = require('inquirer');
 const mysql = require('mysql');
 const department = require('./inq/departmentIQ');
 const employee = require('./inq/employeeIQ');
@@ -16,7 +17,48 @@ connection.connect((err) => {
     console.log(`Connected with id ${connection.threadId}`);
 });
 
-const start = () => {
-    
-    connection.end();
-}
+prompt(
+    [
+        {
+            name: 'choice',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                { name: 'View All Employees', value: 'viewEmp' },
+                { name: 'Add Employees', value: 'addEmp' },
+                { name: 'View Departments', value: 'viewDep' },
+                { name: 'Add Departments', value: 'addDep' },
+                { name: 'View All Roles', value: 'viewRol' },
+                { name: 'Add Roles', value: 'addRol' },
+                { name: 'Update Employee Roles', value: 'updRol' },
+                { name: 'Exit Program', value: 'exit' }
+            ]
+        }
+    ]).then(results => {
+        switch (results.choices) {
+            case 'viewEmp':
+                viewEmployee();
+                break;
+            case 'addEmp':
+                addEmployee();
+                break;
+            case 'viewDep':
+                viewDepartment();
+                break;
+            case 'addDep':
+                addDepartment();
+                break;
+            case 'viewRol':
+                viewRoles();
+                break;
+            case 'addRol':
+                addRoles();
+                break;
+            case 'updRol':
+                updateRoles();
+                break;
+            case 'exit':
+                connection.end();
+                break;
+        }
+    });
